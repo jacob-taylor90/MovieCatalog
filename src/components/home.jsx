5aac579879681664115aa14493658cde873fb896
Jacob Taylor <jacobtaylor@Jacobs-MacBook-Pro.local>
2018-05-10 22:26:31 -0500

    checking git

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,6 +6,7 @@ class Home extends Component {
             <div>
                 <h2>Welcome!</h2>
                 <p>This is your personal movie database where you can add, view, and manage your catalog of movies.</p>
+                <p>Please log in or create an account in order to use the application.</p>
             </div>
         );
     }
